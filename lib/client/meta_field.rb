eefa21597a5c9e762ff8a5d2731da92be5164ace
mrogers-5s <mrogers@weare5stones.com>
2020-03-10 14:45:04 -0500

    feat(client/*,agent,mapper/*): Add support for metafields
    Add additional support for setting custom meta fields

---
+++ lib/client/meta_field.rb
@@ -0,0 +1,49 @@
+module BigcommerceProductAgent
+    module Client
+        class MetaField < AbstractClient
+            @uri_base = 'catalog/products/:product_id/metafields/:meta_field_id'
+
+            def get_for_product(product_id)
+              return [] if product_id.blank?
+
+              response = client.get(uri(product_id: product_id))
+              return response.body['data']
+            end
+
+            def create(meta_field)
+              response = client.post(
+                uri(product_id: meta_field[:resource_id]),
+                meta_field.to_json
+              )
+
+              return response.body['data']
+            end
+
+            def update(meta_field)
+              response = client.put(
+                uri(product_id: meta_field[:resource_id], meta_field_id: meta_field[:id]),
+                meta_field.to_json
+              )
+
+              return response.body['data']
+            end
+
+            def upsert(meta_field)
+              begin
+                if meta_field[:id]
+                  return update(meta_field)
+                else
+                  return create(meta_field)
+                end
+              rescue Faraday::Error::ClientError => e
+                puts e.inspect
+                raise e
+              end
+            end
+
+            def delete(product_id, meta_field_id)
+                client.delete(uri(product_id: product_id, meta_field_id: meta_field_id))
+            end
+        end
+    end
+end
